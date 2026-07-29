9388ad029b70958cd00e87e51a68bcffbbab9389
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-07-29 17:40:59 +0530

    setting up ant design

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "antd";
+import theme from "@/config/themeConfig";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ConfigProvider theme={theme}>
+          <AntdRegistry>
+            {children}
+          </AntdRegistry>
+        </ConfigProvider>
+      </body>
     </html>
   );
 }
---
+++ config/themeConfig.ts
@@ -0,0 +1,13 @@
+import type { ThemeConfig } from 'antd';
+
+const theme: ThemeConfig = {
+  token: {
+    colorPrimary: '#00AFAA',
+    colorTextHeading: '#034144',
+    colorBgLayout: '#F5F8FA',
+    colorText: '#1F2937',
+    fontFamily: 'Inter, sans-serif',
+  },
+};
+
+export default theme;
